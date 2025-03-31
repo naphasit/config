7c26cfd24f5a5d810eec56797081a5ddd959b774
System administrator <naphasitng@gmail.com>
2025-03-31 20:07:44 +0700

    Add Garbage Collector And Flake Support

--- system/default.nix
+++ system/default.nix
@@ -9,5 +9,16 @@
     ./software.nix
   ];
 
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 7d";
+  };
+
+  nix.settings = {
+    auto-optimise-store = true;
+    experimental-features = [ "nix-command" "flakes" ];
+  };
+
   system.stateVersion = "24.11";
 }
