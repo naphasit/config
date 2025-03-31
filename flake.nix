181aa33defe349ad79ee0f6c2f685927086994a2
System administrator <naphasitng@gmail.com>
2025-03-31 20:44:05 +0700

    Setup Home Manager

--- flake.nix
+++ flake.nix
@@ -3,25 +3,20 @@
      
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    impermanence.url = "github:nix-community/impermanence";
+    home-manager.url = "github:nix-community/home-manager";
 
-    impermanence = {
-      url = "github:nix-community/impermanence";
-    };
-
-    # home-manager = {
-    #   url = "github:nix-community/home-manager";
-    #   inputs.nixpkgs.follows = "nixpkgs";
-    # };
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = {nixpkgs, ...} @ inputs:
+  outputs = { nixpkgs, home-manager, ... } @ inputs:
   {
     nixosConfigurations.default = nixpkgs.lib.nixosSystem {
-      specialArgs = {inherit inputs;};
+      specialArgs = {inherit inputs; inherit home-manager;};
       modules = [
         ./system
               
-        # inputs.home-manager.nixosModules.default
+        inputs.home-manager.nixosModules.default
         inputs.impermanence.nixosModules.impermanence
       ];
     };
--- system/users.nix
+++ system/users.nix
@@ -1,7 +1,11 @@
+{ home-manager, ... }:
+
 {
-  users.users.naphasitng = {
+  users.users."naphasitng" = {
     isNormalUser = true;
     extraGroups = [ "wheel" "networkmanager" ];
     hashedPasswordFile = "/persist/passwords/naphasitng";
   };
+
+  home-manager.users."naphasitng" = ../home;
 }
