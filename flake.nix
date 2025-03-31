2ceb658b1df542f5810e9e4f7db8d41847de26f8
System administrator <naphasitng@gmail.com>
2025-03-31 17:55:23 +0700

    Init NixOS Config

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "NixOS Configuration Flake";
+     
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    impermanence = {
+      url = "github:nix-community/impermanence";
+    };
+
+    # home-manager = {
+    #   url = "github:nix-community/home-manager";
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    # };
+  };
+
+  outputs = {nixpkgs, ...} @ inputs:
+  {
+    nixosConfigurations.default = nixpkgs.lib.nixosSystem {
+      specialArgs = {inherit inputs;};
+      modules = [
+        ./configuration.nix
+              
+        # inputs.home-manager.nixosModules.default
+        inputs.impermanence.nixosModules.impermanence
+      ];
+    };
+  };
+}
