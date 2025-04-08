accf4c1e98c41c7a6f106d0972bcea5ccaa0cab5
System administrator <naphasitng@gmail.com>
2025-04-08 19:40:52 +0700

    Feat(Nix): Garbage Collector And Flake Support

--- system/default.nix
+++ system/default.nix
@@ -6,5 +6,16 @@
     ./users.nix
   ];
 
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-then 7d";
+  };
+
+  nix.settings = {
+    auto-optimise-store = true;
+    experimental-features = [ "nix-command" "flakes" ];
+  };
+
   system.stateVersion = "24.11";
 }
