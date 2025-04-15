1e42513b4c449da785e8187ac25e58256ffbd64a
System administrator <naphasitng@gmail.com>
2025-04-15 14:40:00 +0700

    Chore(NixOS): Init NixOS

---
+++ disko.nix
@@ -0,0 +1,58 @@
+{
+  device ? throw "Set this to your disk device, e.g. /dev/sda",
+  ...
+}: {
+  disko.devices = {
+    disk.main = {
+      inherit device;
+      type = "disk";
+      content = {
+        type = "gpt";
+        partitions = {
+          esp = {
+            name = "ESP";
+            size = "512M";
+            type = "EF00";
+            content = {
+              type = "filesystem";
+              format = "vfat";
+              mountpoint = "/boot";
+            };
+          };
+          root = {
+            name = "root";
+            size = "100%";
+            content = {
+              type = "btrfs";
+              extraArgs = ["-f"];
+
+              subvolumes = {
+                "/root" = {
+                  mountpoint = "/";
+                };
+
+                "/persist" = {
+                  mountOptions = ["compress=zstd" "noatime"];
+                  mountpoint = "/persist";
+                };
+
+                "/nix" = {
+                  mountOptions = ["compress=zstd" "noatime"];
+                  mountpoint = "/nix";
+                };
+
+		"/swap" = {
+                  mountpoint = "/swap";
+                  swap = {
+		    swapfile.path = "swapfile";
+                    swapfile.size = "24G";
+		  };
+		};
+              };
+	    };
+          };
+        };
+      };
+    };
+  };
+}
