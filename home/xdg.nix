e14117aea0e7c77603aaaad0df858aad65faa170
System administrator <naphasitng@gmail.com>
2025-05-12 17:03:43 +0700

    Feat(XDG): Remove NixOS Manual

--- home/xdg.nix
+++ home/xdg.nix
@@ -1,9 +1,19 @@
 { pkgs, ... }:
 
 {
-  xdg.portal = {
-    xdgOpenUsePortal = true;
+  xdg = {
     enable = true;
-    extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+    desktopEntries = {
+      nixos-manual = {
+        name = "NixOs Manual";
+        exec = "nixos-help";
+        noDisplay = true;
+      };
+    };
+    portal = {
+      xdgOpenUsePortal = true;
+      enable = true;
+      extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+    };
   };
 }
